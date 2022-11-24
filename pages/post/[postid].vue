c471841c8dbad98a36429dda2f71ea5bdd28f6c2
arunstop <arunstop@gmail.com>
2022-11-24 17:16:59 +0700

    update : added title to post page

--- pages/post/[postid].vue
+++ pages/post/[postid].vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
 import { IPost, IComment } from '~~/composables/types'
+
+
+
 const route = useRoute()
 const promisePost = useFetch<IPost>('https://jsonplaceholder.typicode.com/posts/1')
 const promiseComments = useFetch<IComment[]>(`https://jsonplaceholder.typicode.com/comments?postId=${route.params.postid as string}`)
 const { data: post } = await promisePost
 const { data: comments } = await promiseComments
+const formattedTitle = (str: string) => `${str.charAt(0).toUpperCase()}${str.slice(1, 100)}`
 
+useHead({
+    title: !post.value ?
+        'Reserva - Get your reservation here' :
+        `${formattedTitle(post.value.title)} - Reserva`
+})
 </script>
 <template>
     <div class="flex flex-col gap-2 sm:gap-4">
         <div v-if="!post">Cannot found post with id : {{ $route.params.postid }}</div>
         <PostItem v-else :post="post" />
         <div class="flex flex-col gap-2 sm:gap-4">
-            <CommentItem v-for="comment in comments" :comment="comment"/>
+            <CommentItem v-for="comment in comments" :comment="comment" />
         </div>
     </div>
 </template>
