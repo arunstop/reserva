c280f2b1ec8cbe74e7a4b052d9f7e0066888ad72
arunstop <arunstop@gmail.com>
2022-12-13 22:27:54 +0700

    update : added clearing cart

--- composables/storeCart.ts
+++ composables/storeCart.ts
@@ -1,15 +1,18 @@
 import { IOrder } from './types'
 
-export type  ICartItem = Map<string,IOrder>
-export type ICartState = Map<string,ICartItem>
+export type ICartItem = Map<string, IOrder>
+export type ICartState = Map<string, ICartItem>
 
-export const useCart = () =>
-  useState<ICartState>('cart',()=>new Map ())
-  
-export function cartAdd(key:string,newItem :ICartItem) {
-  useCart().value.set(key,newItem)
+export const useCart = () => useState<ICartState>('cart', () => new Map())
+
+export function cartAdd(key: string, newItem: ICartItem) {
+  useCart().value.set(key, newItem)
 }
 
 export function cartRemove(key: string) {
   useCart().value.delete(key)
+}
+
+export function cartClear() {
+  useCart().value.clear()
 }
