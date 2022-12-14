338bd53da70822b8cb3a4ca3dac5be13278bcc54
arunstop <arunstop@gmail.com>
2022-12-14 22:19:59 +0700

    update : added custom variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './components/**/*.{js,vue,ts}',
-        './layouts/**/*.vue',
-        './pages/**/*.vue',
-        './plugins/**/*.{js,ts}',
-        './nuxt.config.{js,ts}',
-    ],
-    theme: {
-        extend: {
-            gap:{
-                'i': 'inherit'
-            }
-        },
-        fontFamily: {
-            righteous: ['Righteous','Inter', 'system-ui', 'sans-serif'],
-        },
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {
+      gap: {
+        i: 'inherit',
+      },
     },
-    plugins: [],
+    fontFamily: {
+      righteous: ['Righteous', 'Inter', 'system-ui', 'sans-serif'],
+    },
+  },
+  plugins: [
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('tailwindcss/plugin')(function ({ addVariant,matchVariant }) {
+      addVariant('hocus', ['&:hover', '&:focus'])
+      addVariant('children', '&>*')
+      matchVariant('every', (value)=>(`&>${value}`))
+    }),
+  ],
 }
