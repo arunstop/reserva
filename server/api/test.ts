137b30114cb99a825851e57313b72750bc3b74df
arunstop <arunstop@gmail.com>
2022-12-19 20:22:50 +0700

    update : some prisma conf

--- server/api/test.ts
+++ server/api/test.ts
@@ -2,8 +2,12 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
-  const data = await prisma.user.findMany()
-  return {
-    api: data,
-  }
+  const data = await prisma.user.create({
+    data: {
+      email: 'yoag@gmail.com',
+      name: 'Yoga',
+      phone: '123131231313ffffffffffffffffff',
+    },
+  })
+  return data
 })
