d135e3f7edefe2d01a8cc8409058dec6f6046ee6
Arunstop <arunstop@gmail.com>
2023-01-28 11:08:47 +0000

    chore : add user using prisma

--- server/services/userService.ts
+++ server/services/userService.ts
@@ -1,13 +1,32 @@
-import { Prisma } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
+const prisma = new PrismaClient()
 export async function serviceUserAdd(data: Prisma.UserCreateInput) {
+  // mongodb
+  // try {
+  //   const res = await $fetch('/api/user/register', {
+  //     method: 'POST',
+  //     body: data,
+  //   })
+  //   return res
+  // } catch (error) {
+  //   console.log(error)
+  //   return error as string
+  // }
   try {
-    const res = await $fetch('/api/user/register', {
-      method: 'POST',
-      body: data,
+    // create user
+    await prisma.user.create({
+      data,
     })
-    return res
-  } catch (error) {
-    console.log(error)
-    return error as string
+    // get user
+    const newUser = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    })
+    if (!newUser) throw new Error('Cannot find user')
+    return newUser
+  } catch (e) {
+    console.error(e)
+    return null
   }
 }
